9133264d9d8086ec4b87524e1c69ab0fc9187f42
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-01-11 18:39:03 -0300

    first commit, server setup

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import express from 'express'
+import cors from 'cors'
+const PORT = 5000
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
