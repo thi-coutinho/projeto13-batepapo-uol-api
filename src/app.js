30b2e50053d3db56d3c6d03326b269e599ba4a51
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-01-16 10:54:46 -0300

    feat: route get messages with filter for user given in request header

--- src/app.js
+++ src/app.js
@@ -60,23 +60,23 @@ app.get("/participants", async (req, res) => {
 })
 
 app.post('/messages', async (req, res) => {
-    const { to,text,type } = req.body
-    const {user} = req.headers
+    const { to, text, type } = req.body
+    const { user } = req.headers
     const messageSchema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
-        type: joi.string().valid('message','private_message').required()
+        type: joi.string().valid('message', 'private_message').required()
     })
-    const validation = messageSchema.validate({ to,text,type })
+    const validation = messageSchema.validate({ to, text, type })
     if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message);
         return res.status(422).send(errors);
     }
 
     try {
-        const foundUser = await db.collection("participants").findOne({ name:user })
+        const foundUser = await db.collection("participants").findOne({ name: user })
         // const foundUser = await db.collection("participants").findOne({ name:to })
-        if (!foundUser ) return res.status(409).send("Not valid user")
+        if (!foundUser) return res.status(409).send("Not valid user")
         let now = Date.now()
         const message = {
             from: user, to, text, type, time: dayjs(now).format('HH:mm:ss')
@@ -87,4 +87,26 @@ app.post('/messages', async (req, res) => {
         res.status(500).send(error.message)
     }
 
+})
+
+app.get('/messages', async (req, res) => {
+    const { user } = req.headers
+    const limit = parseInt(req.query.limit)
+
+    try {
+        let allMessages = await db.collection("messages").find().toArray()
+        allMessages = allMessages.reverse().filter(m => {
+            let validMessage = (m.type === "message" || m.from === user || m.to === user)
+            return validMessage
+        })
+
+        if (limit) {
+            res.send(allMessages.slice(0, limit))
+        } else {
+            res.send(allMessages)
+        }
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
 })
