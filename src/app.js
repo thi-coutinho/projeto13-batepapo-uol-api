d9ccbd2c852084c305a96b3049dbd9b5fe76c010
thi-coutinho <thi.coutinho.santos@gmail.com>
2023-01-16 10:13:01 -0300

    feat: rout post messages with validation

--- src/app.js
+++ src/app.js
@@ -12,9 +12,14 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-const mongoCilent = new MongoClient(process.env.DATABASE_URL)
-await mongoCilent.connect()
-let db = mongoCilent.db()
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+try {
+    await mongoClient.connect()
+    console.log("MongoDB Connected!")
+} catch (error) {
+    console.log(err.message)
+}
+const db = mongoClient.db()
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
@@ -52,4 +57,34 @@ app.get("/participants", async (req, res) => {
     } catch (error) {
         res.status(500).send(error.message)
     }
+})
+
+app.post('/messages', async (req, res) => {
+    const { to,text,type } = req.body
+    const {user} = req.headers
+    const messageSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().valid('message','private_message').required()
+    })
+    const validation = messageSchema.validate({ to,text,type })
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const foundUser = await db.collection("participants").findOne({ name:user })
+        // const foundUser = await db.collection("participants").findOne({ name:to })
+        if (!foundUser ) return res.status(409).send("Not valid user")
+        let now = Date.now()
+        const message = {
+            from: user, to, text, type, time: dayjs(now).format('HH:mm:ss')
+        }
+        await db.collection("messages").insertOne(message)
+        res.sendStatus(201)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+
 })
